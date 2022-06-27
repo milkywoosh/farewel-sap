f821b6f5467a30b3a84ebedf1864f9f5b2f1ddbd
lukman aul <archer.lukman@gmail.com>
2022-06-27 09:06:23 +0700

    Update dataFramePhoto.js
    add some more text kesan

--- src/Pages/People/FramePhoto/dataFramePhoto.js
+++ src/Pages/People/FramePhoto/dataFramePhoto.js
@@ -24,104 +24,109 @@ export const KKSquad = [
       "Supervisor KK jaman now, selalu bantu anak buahnya semaksimal mungkin apapun masalahnya. Belajar cara manajemen komplen dari client sebanyak apapun clientnya",
   },
   {
-    no: 4,
+    no: 5,
     name: "Bang wangwang",
     text:
       "AKA bang Anwar sanusi, bapak 2 anak yang udah kaya paman sendiri di SAP. Belajar banyak dari bang anwar cara ngomong ke client, , santai tapi tetep profesional",
   },
   {
-    no: 4,
+    no: 6,
     name: "Karimin",
-    text: "Orang bekasi, pens berat padil jaidi kaya nina satiri, anak bontot KK hampir gak pernah keliatan pusing tapi baliknya paling malem, CS setia BRI",
+    text: "Orang bekasi, pens berat padil jaydiyy sama kaya nina satiri, anak bontot KK hampir gak pernah keliatan pusing tapi baliknya paling malem, CS setia BRI",
   },
   {
-    no: 4,
+    no: 7,
     name: "Bang chochooo",
     text: "Dateng paling pagi gak pernah telat padahal rumahnya di keranjiii, DS anti telat, kerja bagai kuda bukan maeeeen",
   },
   {
-    no: 4,
+    no: 8,
     name: "Mba dalll",
     text: "AKA mba dahlia yang kalo udah dikomplen BNI pasti kagak tenang, tambah BCA yang SLAnya kenceng banget",
   },
   {
-    no: 4,
+    no: 9,
     name: "Bang Jagung",
     text: "bang jagunggg AKA Agung Sudana Yoga, sobat pak DONI BCA, bercanda kocag muluww, sharing-sharing komplenan client ",
   },
   {
-    no: 4,
+    no: 10,
     name: "Mba Nopal",
     text: "AKA Nova Mulia Sari, sobat bareng pas baru SAP dulu sampe udah punya anak, dan ternyata gua harus resign duluan yaa cuyy, anti ngomel, diem-diem intel juga cuy bareng zizi, rahma wkwk",
   },
   {
-    no: 4,
-    name: "Mama arka",
+    no: 11,
+    name: "Mama arkahh",
     text:
       "AKA mba Rahma, yang baru banget masuk SAP beberapa bulan yang lalu pasca kholid resign, jago lawak apalagi kalo tandem bang wang-wang",
   },
   {
-    no: 5,
+    no: 12,
     name: "Mba ziii",
     text:
       "mba zizi AKA Fauziyah, mahmud 1 anak yang baik hati tukang bawa makanan banyak, kalo mau ambil di lacinya aja tu gaes tpi ijin dulu yak wkwkwk, hillnya 10 senti paling hitz se jaksel. Besti nya adit juga",
   },
   {
-    no: 5,
+    no: 13,
     name: "Mba duwii",
     text:
       "Tetangga sebelah kanan gua, geprek lover anti bosen, SAT SET tenggo. Dedengkot KK juga, sobat intel sama kaya mba nova sering sharing komplenan klien, sobat mba iis Tada dan Sara",
   },
   {
-    no: 6,
+    no: 14,
     name: "Pa frediyy",
     text:
       "pa freddiiiyy the master of foxpro, manajer + dedengkot KK, slowly but sure penghuni KK... manajer dengan skill warehouse KK/Valuable goods terbaik + software custom SAT SET SAT SET",
   },
   {
-    no: 7,
+    no: 15,
     name: "Wa'awuuww wauw",
+    text:
+      "Wahyu tapi bukan fredi, juga bukan wahyu ras sapi pedaging, adalah Wahyu wibowo, bocah cawank, besti sobat pojok tim KK , calon koordinator retur KK SAP andara, sobat solid cawank.. U'u ma'an umaann ready to berangkat ke puncak suatu saat nanti wkwk    ",
+  },
+   {
+    no: 15,
+    name: "Mba Puut",
     text:
       "Wahyu tapi bukan fredi, juga bukan wahyu ras sapi pedaging, adalah Wahyu wibowo, bocah cawank, besti sobat pojok tim KK , calon koordinator retur KK SAP andara, sobat solid cawank.. U'u ma'an umaann ready to berangkat ke puncak suatu saat nanti wkwk    ",
   },
   {
     no: 8,
-    name: "Goweeedttt",
+    name: "Gowed",
     text:
-      "preman usman harun, ati ati jangan dikateee bos ntar kenape kenape yeee",
+      "AKA Dewi, preman usman harun paling keras bos, sobat solid bareng wauww, pokoknya ati ati jangan dikata bos ntar kenape kenape ",
   },
-  { no: 9, name: "Inang Butet", text: "AKA nopita on ce upon etaymmmmmm" },
+  { no: 9, name: "Inang Butet", text: "AKA nopita on ce upon etaymmmmmm kata wauww" },
   {
     no: 10,
     name: "Nina",
-    text: "Nina atau jenap yg katanya mau ketemuin gua sama cowonya wkwkwk",
+    text: "Nina atau jenap satiri yg katanya mau ketemuin gua sama cowonye, kalo mesen nasi udug, keripik 2 ribuan bisa ke dia cuuy.",
   },
   {
     no: 10,
     name: "Bang Solmet",
     text:
-      "AKA bang Rahmat Nur Hadi, pengguna Foxpro garis keras, aplikasi manajemen retur kelas industri yang kepake banget",
+      "AKA bang Rahmat Nur Hadi yang ngurusin return barang SELINDO, pengguna Foxpro garis keras, aplikasi manajemen retur kelas industri yang kepake banget",
   },
   {
     no: 10,
     name: "Bang Agilll",
     text:
-      "Sobat ngapak BARLINGMASCAKEB, perantau garis keras yang selalu sigap untuk diminta tolong cek barang fisik retur yang nyasar seluruh anak SAP",
+      "Sobat ngapak BARLINGMASCAKEB asal banjarnegara, perantau garis keras yang selalu sigap untuk diminta tolong cek barang fisik retur yang nyasar seluruh anak SAP",
   },
   {
     no: 11,
     name: "Dildulll",
-    text: "emaknya iqizzz, ngeri bingiiiits wkwkwk (ampun cuy)",
+    text: "emaknya iqiz, atiati digalakin euy wkwkwk (ampun cuy)",
   },
   {
     no: 11,
-    name: "Godit bestiee",
-    text: "AKA Adit bucin",
+    name: "Adit",
+    text: "AKA Aditya, orang KK lama sobat pak lucius Maybank, konsultan perdiskonan segala barang, dari gofud atau fud fud-an lainnya, barang stylish dress OOTD, minya wangi sampe Mekanikal Keyboard boleh ni tanya ke doi",
   },
-  { no: 12, name: "Kholidunn", text: "special LEGEND " },
-  { no: 13, name: "Namirah", text: "special LEGEND" },
-  { no: 13, name: "Matunn", text: "special LEGEND" },
-  { no: 14, name: "Bang botak", text: "special LEGEND anak punk TGR" },
+  { no: 12, name: "Kholidunn", text: "special LEGEND Former SAP, AKA Kholid, sobat yang bawa gua masuk ke SAP, terimakasih banyak cuyyy gua pokoknya mah, berpisah untuk ketemu lagi di mawar" },
+  { no: 13, name: "Namirah", text: "special LEGEND Former SAP, yang sekarang di kurir sebelah" },
+  { no: 14, name: "Bang botak", text: "special LEGEND, Former SAP SPV gua jaman baru masuk" },
 ];
 
 export const DataPerson = [
